d62fa48376045cb642f4186ab67c068e54477b2b
marcbull <31337222+marcbull@users.noreply.github.com>
2022-09-09 07:28:08 +0200

    chore: remove comment

--- src/main.rs
+++ src/main.rs
@@ -75,7 +75,6 @@ fn main() -> Result<(), player::PlayerError> {
         canvas.copy(&texture, None, None).unwrap();
 
         canvas.present();
-        //::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
     }
 
     player.stop()?;
